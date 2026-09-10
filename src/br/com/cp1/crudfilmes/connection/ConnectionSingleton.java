2675124e0c793aea497794b20ab02d7bdc79c4bc
labsfiap <matheusegea220504@gmail.com>
2026-09-10 20:38:44 -0300

    adicionando senha no gitignore

--- src/br/com/cp1/crudfilmes/connection/ConnectionSingleton.java
+++ src/br/com/cp1/crudfilmes/connection/ConnectionSingleton.java
@@ -1,8 +1,11 @@
 package br.com.cp1.crudfilmes.connection;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionSingleton {
 
@@ -16,15 +19,20 @@ public class ConnectionSingleton {
         if (connection == null) {
 
             try {
-                String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-                String usuario = "RM563490";
-                String senha = "290506";
+                Properties properties = new Properties();
+
+                FileInputStream arquivo = new FileInputStream("config.properties");
+                properties.load(arquivo);
+
+                String url = properties.getProperty("oracle.url");
+                String usuario = properties.getProperty("oracle.user");
+                String senha = properties.getProperty("oracle.password");
 
                 connection = DriverManager.getConnection(url, usuario, senha);
 
                 System.out.println("Conexao realizada com sucesso!");
 
-            } catch (SQLException e) {
+            } catch (IOException | SQLException e) {
                 throw new RuntimeException("Erro ao conectar com o banco de dados.", e);
             }
         }
